3f2d57fcd214f1bb6fc6be87ca02d9d72c94795a
vincent <vincentuong@yahoo.com>
2022-09-16 23:35:51 -0500

    index js update

--- src/index.js
+++ src/index.js
@@ -5,16 +5,30 @@ import { Provider } from 'react-redux';
 import { Provider as AlertProvider } from 'react-alert';
 import AlertTemplate from 'react-alert-template-basic';
 import { createStore, applyMiddleware } from 'redux';
-import { devToolsEnhancer } from 'redux-devtools-extension/logOnlyInProduction';
+// import { composeWithDevTools } from 'redux-devtools-extension';
 import './index.css';
 import App from './App';
 import thunk from 'redux-thunk';
 import { rootReducer } from './reducers/rootReducer'
 
-const store = createStore(
-  rootReducer,
-  devToolsEnhancer(applyMiddleware(thunk)),
+const composeEnhancers =
+  typeof window === 'object' &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+      // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+    }) : compose;
+
+const enhancer = composeEnhancers(
+  applyMiddleware(thunk),
+  rootReducer
+  // other store enhancers if any
 );
+const store = createStore(reducer, enhancer);
+
+// const store = createStore(
+//   rootReducer,
+//   composeWithDevTools(applyMiddleware(thunk)),
+// );
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
